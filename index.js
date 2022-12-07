c25ae0ecbdbab481c0a66578841d696a8539664d
Breno Santana <brenosantanabruno@live.com>
2022-12-07 16:05:49 -0300

    update to expo

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import { registerRootComponent } from 'expo';
+/**
+ * @format
+ */
 
-import App from './App';
+import { AppRegistry, Text } from 'react-native';
+import Navigator from './src/Navigator';
+import { name as appName } from './app.json';
+import App from './src/App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+
+
+AppRegistry.registerComponent(appName, () => Navigator)
+
+
+
